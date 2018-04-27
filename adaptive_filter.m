8dd4db8d19446d2a95a5a2822b237ba1b7814839
samyak agrawal <samagrawal.197@gmail.com>
2018-04-28 01:40:52 +0530

    changed rls filter

--- adaptive_filter.m
+++ adaptive_filter.m
@@ -1,10 +1,6 @@
 function [y] = adaptive_filter(PPG,X,Y,Z)
-	temp = zeros(1,1000);
-    temp;
-	P0  = 10*eye(11,11);
-	rlsFilt = dsp.RLSFilter(11,'InitialInverseCovariance',P0);
-     % disp('HEYYY');
-%	rlsFilt= dsp.RLSFilter(11, ’ForgettingFactor’ ,0.99,'InitialInverseCovariance', p0);
+	temp = zeros(1,length(PPG)) ;
+	rlsFilt = dsp.RLSFilter(69 ,'ForgettingFactor', 0.990) ;
 	[, err] = rlsFilt(X,PPG);
     temp = temp + err;
 	[, err] = rlsFilt(Y,PPG);
--- band_pass.m
+++ band_pass.m
@@ -2,11 +2,11 @@ function PPG1 = band_pass(PPG,sample_rate)
 
 %Bandpass Filter: 0.4 to 3.5Hz
 %
-    ff = fft(PPG,1024);
+    ff = fft(PPG);
     n = 0:1023;
     np = n*2*pi/1024;
     ff(np < 2*pi*0.4/sample_rate | np > 2*pi*(1-0.4/sample_rate)) = 0;
     ff(np > 2*pi*3.5/sample_rate & np < 2*pi*(1-3.5/sample_rate)) = 0;
     PPG1 = ifft(ff);
-    PPG1 = real(PPG1(1:1000));
+    PPG1 = real(PPG1);
 end
